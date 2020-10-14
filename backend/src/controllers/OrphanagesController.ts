b3629fe30d91ef2f256fcaf287f3552118eb9ddf
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-10-14 18:30:38 -0300

    add: routes and controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return res.json(orphanages);
+  },
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return res.json(orphanage);
+  },
+
+  async create(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+      ...req.body,
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.status(201).json(orphanage);
+  },
+};
