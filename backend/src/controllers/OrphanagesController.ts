1934334bdc6ec9657c58c341fc24f618024d0b18
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-10-16 17:20:43 -0300

    refactor: orphanages controller with validate

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 
+import * as Yup from 'yup';
+
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
 
@@ -56,6 +58,25 @@ export default {
       open_on_weekends: open_on_weekends === 'true',
       images,
     };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
+    });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    });
 
     const orphanage = orphanagesRepository.create(data);
 
