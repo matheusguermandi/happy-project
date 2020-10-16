40bd6bb3ad846d8e908d321aded34899a44878f7
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-10-16 17:16:07 -0300

    add:handler errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+
 import path from 'path';
 import cors from 'cors';
 
 import './database/connection';
 
 import routes from './routes';
+import errors from './errors/handler';
 
 const app = express();
 
@@ -12,5 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errors);
 
 app.listen(3333);
