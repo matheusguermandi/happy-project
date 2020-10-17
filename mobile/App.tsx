c2ff7ba06effa9a5a3ac6b32a48794f2d44dfb73
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-10-17 15:20:18 -0300

    add: screen orphanages map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from './src/images/map-marker.png';
+
+import { 
+  Nunito_600SemiBold, 
+  Nunito_700Bold, 
+  Nunito_800ExtraBold, 
+  useFonts
+} from "@expo-google-fonts/nunito";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if (!fontsLoaded)  return null;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -23.6603332,
+          longitude: -46.6937712,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}>
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+          coordinate={{
+            latitude: -23.6603332,
+            longitude: -46.6937712,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}> teste </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>N Orfanatos encontrados</Text>
+        
+        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+          <Feather name='plus' size={20} color='#fff' />
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +67,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: '100%'
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold'
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3
+  },
+
+  footerText: {
+    fontFamily: 'Nunito_700Bold',
+    color: '#8fa7b3'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
