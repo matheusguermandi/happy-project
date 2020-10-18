2421712b8d0d1f1b7950b2c16ad82ac04e54846b
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-10-18 10:32:06 -0300

    add: routes

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
 
 import OrphanagesMap from "./screens/OrphanagesMap";
+import OrphanageDetails from "./screens/OrphanageDetails";
+import SelectMapPosition from "./screens/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./screens/CreateOrphanage/OrphanageData";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -10,7 +13,25 @@ const Routes: React.FC = () => {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
-        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen
+          name="OrphanagesMap"
+          component={OrphanagesMap}
+        />
+
+        <Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+        />
+
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+        />
+
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+        />
       </Navigator>
     </NavigationContainer>
   );
